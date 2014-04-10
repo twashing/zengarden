d405f994a9b9ac128d6cccb1e8ad0686bf7e2fb3
Timothy Washington <twashing@gmail.com>
2014-04-10 13:52:06 -0700

    -- adding context passing to the tree-walker -- full path finding for multiple subling div classes / ids -- adding this changes file to .gitignore

--- src/zengarden/one.clj
+++ src/zengarden/one.clj
@@ -2,6 +2,8 @@
   (:require [clojure.zip :as zip]))
 
 
+(declare walka walkb)
+
 (def u [[:html {:height "100%" :display "flex"}
          [:body {:display "flex"}
           [:.myclass {:float "left"}]]
@@ -9,26 +11,28 @@
 
 (def v [[:html {:height "100%" :display "flex"}
          [:body {:display "flex"}
-          [:.myclass {:float "left"}]]]])
+          [:.herclass :.hisclass {:float "left"}
+           [:.thisclass {:color "white"}
+            [:#anid {:color "blue"}]]]]]])
 
 (def w [[:html {:height "100%" :display "flex"}
          [:body {:display "flex"}]]])
 
 
-(declare walka walkb)
+(defn walka [clist context]
 
-(defn walka [clist]
-  (loop [cl clist]
+  (loop [cl clist ctx context]
+
     (let [node (first cl)
           remaining (rest cl)]
 
-      (println "node[" node "]")
-      (walkb node)
+      (println "node[" node "] / context[" ctx "]")
+      (walkb node ctx)
       (if-not (empty? remaining)
-        (recur remaining)))))
+        (recur remaining ctx)))))
 
 
-(defn walkb [node]
+(defn walkb [node context]
 
   (let [elements (filter keyword? node)
         attrs (first (filter map? node))
@@ -37,7 +41,16 @@
     (println "elements[" elements "]")
     (println "attributes[" attrs "]")
     (println "children[" children "]")
+    (loop [elems elements]
+      (let [eelem (first elems)
+            relem (rest elems)]
+        (println "... each element[" eelem "] / context[" context "]")
+        (if-not (empty? relem)
+          (recur relem))))
     (println)
 
     (if-not (empty? children)
-      (walka children))))
+      (walka children (if (= 1 (count elements))
+                        (concat context elements)
+                        (conj (into [] context)
+                              (into [] elements)))))))
