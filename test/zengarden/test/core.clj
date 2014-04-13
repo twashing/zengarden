9ca720f4c666aa3229ff17038431821d94a6170c
Timothy Washington <twashing@gmail.com>
2014-04-13 01:10:35 -0700

    -- adding more example edn test files -- adding a test for pseudo classes

--- test/zengarden/test/core.clj
+++ test/zengarden/test/core.clj
@@ -54,4 +54,24 @@
           r0 (zc/css (:m styles))
           c0 "\n\nhtml{ \n  height : 100%; \n  display : flex; }\nsvg:not(:root) {\n  overflow: hidden;\n}"]
 
+      (is (= r0 c0))))
+
+  (testing "pseudoclasses"
+
+    (let [i0 [[:.flex-item:nth-child  {:-webkit-box-ordinal-group "1"
+                                       :-moz-box-ordinal-group "1"
+                                       :-webkit-order "0"
+                                       :-ms-flex-order "0"
+                                       :order "0"
+                                       :-webkit-box-flex "0"
+                                       :-moz-box-flex "0"
+                                       :-webkit-flex "0 1 auto"
+                                       :-ms-flex "0 1 auto"
+                                       :flex "1 1 auto"
+                                       :-webkit-align-self "stretch"
+                                       :-ms-flex-item-align "stretch"
+                                       :align-self "stretch"}]]
+          r0 (zc/css i0)
+          c0 "\n\n.flex-item:nth-child{ \n  -webkit-flex : 0 1 auto; \n  align-self : stretch; \n  -webkit-box-ordinal-group : 1; \n  flex : 1 1 auto; \n  -webkit-box-flex : 0; \n  -webkit-align-self : stretch; \n  -ms-flex-item-align : stretch; \n  -moz-box-ordinal-group : 1; \n  -ms-flex-order : 0; \n  order : 0; \n  -moz-box-flex : 0; \n  -webkit-order : 0; \n  -ms-flex : 0 1 auto; }"]
+
       (is (= r0 c0)))))
