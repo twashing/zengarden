21c1a14c8ab26043dd3112481b42c6d053d66d51
Timothy Washington <twashing@gmail.com>
2014-04-13 03:10:06 -0700

    lein-release plugin: preparing 0.1.5 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zengarden "0.1.5-SNAPSHOT"
+(defproject zengarden "0.1.5"
   :description "Zengarden is a library for rendering CSS in Clojure."
   :url "https://github.com/twashing/zengarden"
   :license {:name "Eclipse Public License"
